9a48deef2c94f975b3faa31be0e929bf89a5dca6
weskiller <weskiller@vip.qq.com>
2021-11-05 20:21:34 +0800

    [perf] allow Hyperf\HttpMessage\Server\Request instead of Hyperf\HttpMessage\Server\Request pass to middleware

--- src/Http/CoreMiddleware.php
+++ src/Http/CoreMiddleware.php
@@ -6,10 +6,21 @@ declare(strict_types=1);
 namespace Weskiller\HyperfMiddleware\Http;
 
 use FastRoute\Dispatcher;
+use Hyperf\HttpMessage\Server\Request;
 use Hyperf\HttpServer\CoreMiddleware as HttpCoreMiddleware;
+use Psr\Http\Message\ServerRequestInterface;
 use RuntimeException;
+
+
 class CoreMiddleware extends HttpCoreMiddleware
 {
+    public function dispatch(ServerRequestInterface $request): ServerRequestInterface
+    {
+        $context = parent::dispatch($request);
+        //allow Hyperf\HttpMessage\Server\Request instead of Hyperf\HttpMessage\Server\Request pass to middleware
+        return $request instanceof Request ? $context : $request;
+    }
+
     protected function createDispatcher(string $serverName): Dispatcher
     {
         $factory = $this->container->get(DispatcherFactory::class);
